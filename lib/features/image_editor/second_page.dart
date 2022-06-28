80919736d6b797a0c6a0c2dbea1fd60f838a72da
sthakrishnakumar <sthakrishnakumar@gmail.com>
2022-06-28 12:54:58 +0545

    share option added

--- lib/features/image_editor/second_page.dart
+++ lib/features/image_editor/second_page.dart
@@ -1,10 +1,13 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:screenshot/screenshot.dart';
+import 'package:share_plus/share_plus.dart';
 
 class SecondPage extends StatefulWidget {
   final Uint8List imageData;
@@ -38,16 +41,17 @@ class _SecondPageState extends State<SecondPage> {
       appBar: AppBar(
         title: const Text('title'),
         actions: [
+          IconButton(
+            onPressed: () {
+              saveandShare(widget.imageData);
+            },
+            icon: const Icon(Icons.share),
+          ),
           ElevatedButton(
             onPressed: () {
               // final image = await controller.captureFromWidget(imageWidget());
               if (widget.imageData.isNotEmpty) {
                 saveImage(widget.imageData);
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text('Image Saved Succesfully'),
-                  ),
-                );
               }
             },
             child: const Text('Save Image'),
@@ -66,6 +70,19 @@ class _SecondPageState extends State<SecondPage> {
         .replaceAll(':', '-');
     final name = 'Image $time';
     final result = await ImageGallerySaver.saveImage(bytes, name: name);
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Image Saved Succesfully'),
+      ),
+    );
     return result['filePath'];
+  }
+
+  Future saveandShare(Uint8List bytes) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final image = File('${directory.path}/flutter.png');
+    image.writeAsBytesSync(bytes);
+    await Share.shareFiles([image.path]);
   }
 }
